670fce45260b87f2012ebf718d941dbcb4e1d9e8
Victorque <78320258+CorpulentAxolotl@users.noreply.github.com>
2025-10-29 21:46:58 -0700

    created binarySearch.py

---
+++ binarySearch.py
@@ -0,0 +1,17 @@
+sorted_list = [1, 4, 7, 9, 11, 34, 78, 80, 100]
+
+left = 0
+right = len(sorted_list) - 1 # 8
+target_value = 78
+
+while sorted_list[left] != target_value:
+    test_index = (left + right)/2
+    test_index = int(test_index) # round down
+
+    test_value = sorted_list[test_index]
+
+    if test_value > target_value:
+        right = test_index
+    else:
+        left = test_index
+print(left)
